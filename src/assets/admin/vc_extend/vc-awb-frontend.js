d0d59cbb191d90e13e60757585a7daced4538f9d
nk-o <nkdevinfo@gmail.com>
2017-04-21 21:52:54 +0300

    reformatted again js code added click action on vc awn icon

--- src/assets/admin/vc_extend/vc-awb-frontend.js
+++ src/assets/admin/vc_extend/vc-awb-frontend.js
@@ -54,52 +54,58 @@ jQuery(function ($) {
                 $icon = e.view.$controls_buttons.parent().children('.vc_control-awb');
             }
 
-            if (params && (params.awb_type)) {
-
-                // add indicator to row or column
-                if (!$icon.length && (e.attributes.shortcode === 'vc_row' || e.attributes.shortcode === 'vc_column'))  {
-                    $icon = $('<span class="vc_control-awb">').appendTo(e.view.$controls_buttons.parent());
-                }
-
-                // insert overlay color
-                if ($icon) {
-                    $icon.html('<span class="vc_control-awb-overlay" style="background-color: ' + (params.awb_color || 'transparent') + ';"></span>');
-                }
-
-                // update image thumbnail
-                $icon.css('background-image', '');
-
-                if (params.awb_image) {
-                    var $model = $("[data-model-id=" + e.id + "]");
-                    var image_src = $model.data("field-awb_image-attach-image");
-                    var $post_id = $("#post_ID");
-                    var post_id = $post_id.length ? $post_id.val() : 0;
-
-                    switch (e.getParam("source")) {
-                        case "external_link":
-                            $icon.css('background-image', 'url("' + e.getParam("custom_src") + '")');
-                            break;
-                        default:
-                            _.isEmpty(params.awb_image) && "featured_image" !== e.getParam("source") ? _.isUndefined(image_src) || ($model.removeData("field-awb_image-attach-image"), $icon.css('background-image', 'url("' + image_src + '")')) : $.ajax({
-                                type: "POST",
-                                url: window.ajaxurl,
-                                data: {
-                                    action: "wpb_single_image_src",
-                                    content: params.awb_image,
-                                    params: e.attributes.params,
-                                    post_id: post_id,
-                                    _vcnonce: window.vcAdminNonce
-                                },
-                                dataType: "html",
-                                context: e.view
-                            }).done(function(image_src) {
-                                var image_exists = image_src.length || "featured_image" === e.getParam("source");
-                                $icon.css('background-image', 'url("' + image_src + '")')
-                            })
-                    }
-                }
-            } else {
+            // prevent if disabled awb
+            if (!params || !params.awb_type) {
                 $icon.css('background-image', '').html('');
+                if ($icon.hasClass('vc_control-awb')) {
+                    $icon.remove();
+                }
+                return;
+            }
+
+
+            // add indicator to row or column
+            if (!$icon.length && (e.attributes.shortcode === 'vc_row' || e.attributes.shortcode === 'vc_column'))  {
+                $icon = $('<span class="vc_control-awb">').appendTo(e.view.$controls_buttons.parent());
+            }
+
+            // add edit control attribute
+            $icon.attr('data-vc-control', 'edit');
+
+            // insert overlay color
+            $icon.html('<span class="vc_control-awb-overlay" style="background-color: ' + (params.awb_color || 'transparent') + ';"></span>');
+
+            // update image thumbnail
+            $icon.css('background-image', '');
+
+            if (params.awb_image) {
+                var $model = $("[data-model-id=" + e.id + "]");
+                var image_src = $model.data("field-awb_image-attach-image");
+                var $post_id = $("#post_ID");
+                var post_id = $post_id.length ? $post_id.val() : 0;
+
+                switch (e.getParam("source")) {
+                    case "external_link":
+                        $icon.css('background-image', 'url("' + e.getParam("custom_src") + '")');
+                        break;
+                    default:
+                        _.isEmpty(params.awb_image) && "featured_image" !== e.getParam("source") ? _.isUndefined(image_src) || ($model.removeData("field-awb_image-attach-image"), $icon.css('background-image', 'url("' + image_src + '")')) : $.ajax({
+                            type: "POST",
+                            url: window.ajaxurl,
+                            data: {
+                                action: "wpb_single_image_src",
+                                content: params.awb_image,
+                                params: e.attributes.params,
+                                post_id: post_id,
+                                _vcnonce: window.vcAdminNonce
+                            },
+                            dataType: "html",
+                            context: e.view
+                        }).done(function(image_src) {
+                            var image_exists = image_src.length || "featured_image" === e.getParam("source");
+                            $icon.css('background-image', 'url("' + image_src + '")')
+                        })
+                }
             }
         });
     }
