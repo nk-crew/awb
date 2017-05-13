3c7c612ddf7a3487f208e0b9b452634904c47352
nk-o <nkdevinfo@gmail.com>
2017-05-13 19:40:33 +0300

    version 1.2.3

--- src/advanced-wordpress-backgrounds.php
+++ src/advanced-wordpress-backgrounds.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:  Advanced WordPress Backgrounds
  * Description:  Parallax, Video, Images Backgrounds
- * Version:      1.2.2
+ * Version:      1.2.3
  * Author:       nK
  * Author URI:   https://nkdev.info
  * License:      GPLv2 or later
