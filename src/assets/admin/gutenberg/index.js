e89ef248f83bc2c28073f46133fa18ef80c29f41
Nikita <nkdevinfo@gmail.com>
2022-02-18 11:33:35 +0300

    removed usage of DOMContentLoaded for register block

--- src/assets/admin/gutenberg/index.js
+++ src/assets/admin/gutenberg/index.js
@@ -9,7 +9,4 @@ import './extensions/ghostkit-grid';
 
 const { registerBlockType } = wp.blocks;
 
-// Previously we used the jQuery's 'ready' event, but it was conflicting with PublishPress Blocks plugin.
-document.addEventListener('DOMContentLoaded', () => {
-  registerBlockType(block.name, block.settings);
-});
+registerBlockType(block.name, block.settings);
