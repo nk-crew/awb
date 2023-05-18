f360850cef9bc84a023e71b369eec4f0747860ac
Nikita <nkdevinfo@gmail.com>
2023-05-18 16:05:26 +0300

    fixed JS error inside iframe in block editor

--- src/assets/awb/utils/get-data.js
+++ src/assets/awb/utils/get-data.js
@@ -1,4 +1,4 @@
-const { AWB } = window;
+const { AWB = {} } = window;
 
 // Thanks https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser/9851769
 const isMobile = /Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/g.test(
